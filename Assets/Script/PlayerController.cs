9252340dd5b2e1ccc66b457c44a9c07451a1e44c
정구홍 <chrtfro98@gmail.com>
2023-10-26 00:17:49 +0900

    Map and Cinemachine

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    float speed = 10.0f;
+    float speed = 30.0f;
     // Start is called before the first frame update
     void Start()
     {
