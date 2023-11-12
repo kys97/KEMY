dbd3bbdb5731a72aa7b8b3d7d8ad411d95caf732
정구홍 <chrtfro98@gmail.com>
2023-11-13 05:04:25 +0900

    game UI

---
+++ Assets/Scripts/RunGame/Collectables/LevelDistance.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LevelDistance : MonoBehaviour
+{
+    public GameObject disDisplay;
+    public int disRun;
+    public bool addingDis = false;
+
+    void Update()
+    {
+        if (addingDis == false)
+        {
+            addingDis = true;
+            StartCoroutine(AddingDis());
+        }
+    }
+
+    IEnumerator AddingDis()
+    {
+        disRun += 1;
+        disDisplay.GetComponent<TextMeshProUGUI>().text = "" + disRun;
+        yield return new WaitForSeconds(0.25f);
+        addingDis = false;
+    }
+}
