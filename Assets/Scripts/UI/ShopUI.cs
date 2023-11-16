c9d15ed0fe21288b5146eee9d731eded2fd84746
kys97 <81239623+kys97@users.noreply.github.com>
2023-11-17 00:11:08 +0900

    Shop UI 마무리

--- Assets/Scripts/UI/ShopUI.cs
+++ Assets/Scripts/UI/ShopUI.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopUI : MonoBehaviour
 {
     void Start()
     {
-        GameManager.Instance.UImanager.BottomOutUI(GameObject.FindGameObjectWithTag("Bottom"));
-        GameManager.Instance.UImanager.RightInUI(gameObject);
+        transform.GetChild(0).GetComponent<Button>().onClick.AddListener(CloseShop);
     }
 
     void Update()
     {
         
+    }
+
+    void CloseShop()
+    {
+        GameManager.Instance.UImanager.UIsetting(Define.ui_level.Lev2, Define.ui.Home);
+        GameObject.FindGameObjectWithTag("Character").transform.GetChild(0).gameObject.SetActive(true);
+        GameObject.FindGameObjectWithTag("Lev1").transform.GetChild(0).GetChild(2).gameObject.SetActive(true);
     }
 }
