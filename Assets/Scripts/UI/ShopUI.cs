afcf1eb66814e08d8a0dd05197f25bf4160b7154
kys97 <lolla44@naver.com>
2023-11-23 06:34:24 +0900

    Shop 작업중

--- Assets/Scripts/UI/ShopUI.cs
+++ Assets/Scripts/UI/ShopUI.cs
@@ -6,13 +6,19 @@ using UnityEngine.UI;
 
 public class ShopUI : MonoBehaviour
 {
+    [SerializeField] private Dictionary<string,int> RandomItem;
+
+    private int tot;
+    private Image ItemEggImage, ItemImage;
+
     void Start()
     {
-        transform.GetChild(0).GetComponent<Button>().onClick.AddListener(CloseShop);
-    }
+        transform.GetChild(0).GetChild(0).GetComponent<Button>().onClick.AddListener(CloseShop);
+        ItemEggImage = transform.GetChild(0).GetChild(1).GetChild(0).GetComponent<Image>();
+        transform.GetChild(0).GetChild(2).GetComponent<Button>().onClick.AddListener(ItemEggOpen);
 
-    void Update()
-    {
+        ItemImage = transform.GetChild(1).GetChild(0).GetChild(0).GetComponent<Image>();
+
         
     }
 
@@ -21,5 +27,12 @@ public class ShopUI : MonoBehaviour
         GameManager.Instance.UImanager.UIsetting(Define.ui_level.Lev2, Define.ui.Home);
         GameObject.FindGameObjectWithTag("Character").transform.GetChild(0).gameObject.SetActive(true);
         GameObject.FindGameObjectWithTag("Lev1").transform.GetChild(0).GetChild(2).gameObject.SetActive(true);
+    }
+
+    void ItemEggOpen()
+    {
+        //Item È®·ü °è»ê
+        int rand = Random.Range(0, tot);
+
     }
 }
