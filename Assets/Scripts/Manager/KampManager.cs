606d1af7ae43710e3956902f7106f153a3c3b01c
kys97 <81239623+kys97@users.noreply.github.com>
2023-12-08 01:41:33 +0900

    동기화 코드 수정

--- Assets/Scripts/Manager/KampManager.cs
+++ Assets/Scripts/Manager/KampManager.cs
@@ -5,29 +5,27 @@ using UnityEngine;
 
 public class KampManager : MonoBehaviourPunCallbacks
 {
-    
-    void Awake()
+    private void Start()
     {
-        
+        //UI Set
+        GameManager.Instance.UImanager.UIsetting(Define.ui_level.Lev1, Define.ui.Kamp);
     }
 
-    private void Start()
+    public override void OnJoinedRoom()
     {
         //Avatar Set
         GameObject MyAvatar = PhotonNetwork.Instantiate("Prefabs/KampCat", Vector3.zero, Quaternion.identity);
-
+        //Skin Set
         Material[] skin = MyAvatar.transform.GetChild(2).GetChild(0).GetComponent<SkinnedMeshRenderer>().materials;
         skin[0] = GameManager.Instance.Resourcesmanager.ItemMaterials[GameManager.Instance.Data.avatar_info.skin];
         skin[1] = GameManager.Instance.Resourcesmanager.ItemMaterials[GameManager.Instance.Data.avatar_info.face];
         MyAvatar.transform.GetChild(2).GetChild(0).GetComponent<SkinnedMeshRenderer>().materials = skin;
-
+        //Transform Set
         Transform parent = GameObject.FindGameObjectWithTag("Character").transform;
         MyAvatar.transform.parent = parent;
         MyAvatar.transform.position = parent.position;
         MyAvatar.transform.localScale = new Vector3(1, 1, 1);
-        
+        //Name Set
 
-        //UI Set
-        GameManager.Instance.UImanager.UIsetting(Define.ui_level.Lev1, Define.ui.Kamp);
     }
 }
